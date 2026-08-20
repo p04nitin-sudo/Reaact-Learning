63a2169b9dc648f4bc11cbbfa50cf92564fe7731
NITIN PANDEY <p04nitin@gmail.com>
2026-08-20 13:11:04 +0530

    Add files via upload

---
+++ profile.jsx
@@ -0,0 +1,12 @@
+import React, { useContext } from 'react'
+import themeContext from './context/createContext'
+
+
+function Profile() {
+  const {user}=useContext(themeContext);
+  if(!user) return <div>please login</div>
+
+    return <div>welcome {user}</div>
+}
+
+export default Profile
